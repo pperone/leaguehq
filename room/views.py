eae15eeacf6ec85c3127310e33b19a1941c2e3a9
Pedro Perone <pedroperone@MacBook-Pro-de-Pedro.local>
2018-12-07 15:17:02 -0300

    Retrieving only corresponding board posts by league

--- room/views.py
+++ room/views.py
@@ -291,7 +291,7 @@ def get_winner(season, leagueId):
 
 # Retrieve past winners
 def get_messages(leagueId):
-    qs = Board.objects.filter(leagueId)
+    qs = Board.objects.filter(leagueId=leagueId)
     ordered = qs.order_by('-id')
     return ordered
 
