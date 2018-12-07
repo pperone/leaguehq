140cfa8d117a8771132544321ebe228e60daa527
Pedro Perone <pedroperone@MacBook-Pro-de-Pedro.local>
2018-12-07 10:17:33 -0300

    Switch to Postgres

--- LeagueHQ/settings.py
+++ LeagueHQ/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'LeagueHQ.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'leaguehq',
+        'USER': 'leaguehq',
+        'PASSWORD': 'Hurling27',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
